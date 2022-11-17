b6995935eb5d42f1276778f4d35c99189e4c0727
Alex Z <azhdan073@gmail.com>
2022-11-17 14:28:57 +0300

    do the basics

---
+++ eveningpicker.py
@@ -0,0 +1,9 @@
+#doing that evening planner thing
+
+from random import choice
+
+#do the list
+games = ['FFXIV', 'Tekken', 'GGST', 'Pathfinder', 'SFV', 'Rimworld']
+
+#print a random game
+print (choice(games))
