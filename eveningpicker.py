69bdabf68a36e12481f25f69cd1d9b57ec444e6b
Alex Z <azhdan073@gmail.com>
2022-11-17 15:07:24 +0300

    randomer random

--- eveningpicker.py
+++ eveningpicker.py
@@ -3,7 +3,28 @@
 from random import choice
 
 #do the list
-games = ['FFXIV', 'Tekken', 'GGST', 'Pathfinder', 'SFV', 'Rimworld', 'Hunt: Showdown']
+#games = ['FFXIV', 'Tekken', 'GGST', 'Pathfinder', 'SFV', 'Rimworld', 'Hunt: Showdown']
+#we're gonna need a bigger list
+games = [['FFXIV', 'mumorpeger', 'Tank'],
+    ['FFXIV', 'mumorpeger', 'deeps'],
+    ['FFXIV', 'mumorpeger', 'healslut'],
+    ['Tekken', 'Fightan', 'Leo'],
+    ['Tekken', 'Fightan', 'Armor King'],
+    ['GGST', 'Fightan', 'Sol'],
+    ['GGST', 'Fightan', 'Ky'],
+    ['GGST', 'Fightan', 'Gio'],
+    ['Pathfinder', 'CRPG', 'Continue'],
+    ['SFV', 'Fightan', 'Ken'],
+    ['SFV', 'Fightan', 'Ed'],
+    ['Rimworld', 'Chill', 'Continue'],
+    ['Kenshi', 'CRPG', 'Continue'],
+    ['Hunt', 'Dakka', 'Meta'],
+    ['Hunt', 'Dakka', 'Cheapo']]
+
+#but what if i'm not THAT indecisive?
+print('any preferences?')
+mood = input()
 
 #print a random game
-print (choice(games))
+nah = choice(games)
+print('you\'ll play ' + nah[0] + ' ' +nah[2])
